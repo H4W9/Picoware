4f7f2ec205673671f579fb6dcb1df04799fbac85
H4W9 <hoferwade@gmail.com>
2026-07-16 14:15:44 -0500

    Try something

--- src/MicroPython/Pancake/mpconfigboard.h
+++ src/MicroPython/Pancake/mpconfigboard.h
@@ -4,6 +4,11 @@
 
 #define MICROPY_HW_MCU_NAME "ESP32-C5"
 
+// This file REPLACES boards/ESP32_GENERIC_C5/mpconfigboard.h at build time, so
+// anything upstream sets for the C5 has to be repeated here or it is silently
+// lost. Upstream disables machine.I2S on this chip:
+#define MICROPY_PY_MACHINE_I2S (0)
+
 // The board's USB-serial bridge is on the console UART, so the REPL lives there.
 #define MICROPY_HW_ENABLE_UART_REPL (1)
 
